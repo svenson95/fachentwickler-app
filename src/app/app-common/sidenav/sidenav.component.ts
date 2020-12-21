60bb27a22e3640c3d9a0844e370927cdd32eaa99
Sven Brodny <sven.brodny@piobyte.de>
2020-12-21 18:32:43 +0100

    refactor: post-link & sidenav layout (my class section)

--- src/app/app-common/sidenav/sidenav.component.ts
+++ src/app/app-common/sidenav/sidenav.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from '../../models/menu-item';
-import { areas, internal, subjects } from '../../../data/menu-items';
+import { areas, myClass, subjects } from '../../../data/menu-items';
 import { SidenavService } from '../../services/sidenav.service';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
@@ -15,7 +15,7 @@ export class SidenavComponent implements OnInit {
 
   subjectsSection: MenuItem[] = subjects;
   areasSection: MenuItem[] = areas;
-  internalSection: MenuItem[] = internal;
+  myClassSection: MenuItem[] = myClass;
 
   constructor(private sidenavService: SidenavService,
               public breakpointObserver: BreakpointObserver,
