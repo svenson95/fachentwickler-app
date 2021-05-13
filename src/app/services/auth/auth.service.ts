b548d6286a49ee2a00b52e6dc42b1210960de8a0
Sven Brodny <sven.brodny@piobyte.de>
2021-05-13 12:42:08 +0200

    refactor: auth service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -12,183 +12,184 @@ import { LoginResponse, RegisterResponse, LogoutResponse, AuthenticatedResponse,
 import { ThemeService } from '../theme.service';
 import { DataService } from '../data/data.service';
 
-const AUTH_STORAGE_KEY = 'fiappy_auth';
+const CREDENTIALS_STORAGE_KEY = 'fiappy_auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  /* -- Auth API links -- */
-  private loginUrl = `${environment.baseUrl}/user/login`;
-  private registerUrl = `${environment.baseUrl}/user/register`;
-  private editUserUrl = `${environment.baseUrl}/user/edit-user`;
-  private addProgressUrl = `${environment.baseUrl}/user/add-progress`;
-  private logoutUrl = `${environment.baseUrl}/user/logout`;
-  private authenticatedUrl = `${environment.baseUrl}/user/authenticated`;
+    private LOGIN_ENDPOINT = environment.baseUrl + '/user/login';
+    private REGISTER_ENDPOINT = environment.baseUrl + '/user/register';
+    private EDIT_USER_ENDPOINT = environment.baseUrl + '/user/edit-user';
+    private ADD_PROGRESS_ENDPOINT = environment.baseUrl + '/user/add-progress';
+    private LOGOUT_ENDPOINT = environment.baseUrl + '/user/logout';
+    private AUTHENTICATED_ENDPOINT = environment.baseUrl + '/user/authenticated';
 
-  /* -- Auth Service  -- */
-  public user: User;
-  public token = 'jwt';
-  public isAuthenticated = false;
-  public redirectUrl: string;
+    public user: User;
+    public token = 'jwt';
+    public isAuthenticated = false;
+    public redirectUrl: string;
 
-  constructor(private httpClient: HttpClient,
-              private themeService: ThemeService,
-              private dataService: DataService,
-              private router: Router,
-              private matSnackBar: MatSnackBar,
-  ) {
-    this.checkAuthentication();
-  }
+    constructor(private httpClient: HttpClient,
+                private themeService: ThemeService,
+                private dataService: DataService,
+                private router: Router,
+                private matSnackBar: MatSnackBar,
+    ) {
+        this.checkAuthentication();
+    }
 
-  /* -- Auth API requests -- */
-  login(user: AuthUser): Observable<LoginResponse> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      withCredentials: true
-    };
+    login(user: AuthUser): Observable<LoginResponse> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+            withCredentials: true
+        };
 
-    return this.httpClient.post<LoginResponse>(this.loginUrl, JSON.stringify(user), httpOptions)
-      .pipe(map(response => {
-          // console.log('response POST login', response);
-          this.isAuthenticated = response.isAuthenticated;
+        return this.httpClient.post<LoginResponse>(this.LOGIN_ENDPOINT, JSON.stringify(user), httpOptions)
+            .pipe(map(response => {
+                // console.log('response POST login', response);
+                this.isAuthenticated = response.isAuthenticated;
 
-          if (response.isAuthenticated) {
-            this.user = response.user;
-            this.token = response.token;
-            this.storeData();
-          }
-          return response;
-      }));
-  }
+                if (response.isAuthenticated) {
+                    this.user = response.user;
+                    this.token = response.token;
+                    this.storeData();
 
-  register(user: RegisterUser): Observable<RegisterResponse> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      withCredentials: true
-    };
+                    if (this.redirectUrl) {
+                        this.router.navigateByUrl(this.redirectUrl);
+                    }
+                }
+                return response;
+            }));
+    }
 
-    return this.httpClient.post<RegisterResponse>(this.registerUrl, JSON.stringify(user), httpOptions)
-      .pipe(map(response => {
-          // console.log('response POST register', response);
-          this.isAuthenticated = response.success;
-          if (response.success) {
-            this.user = response.user;
-            this.token = response.token;
-            this.storeData();
-          }
-          return response;
-      }));
-  }
+    register(user: RegisterUser): Observable<RegisterResponse> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+            withCredentials: true
+        };
 
-  /* -- Get all lessons for user progress analysis (lesson progress & next lesson) -- */
-  fetchAllLessons(): void {
-    this.dataService.getAllLessons().subscribe(
-        (lessons) => {
-          this.dataService.dashboard.allLessons = lessons;
-          this.fetchNextLesson(lessons);
-        }, (error) => {
-          console.log('error while GET all-lessons', error);
-        }
-    );
-  }
+        return this.httpClient.post<RegisterResponse>(this.REGISTER_ENDPOINT, JSON.stringify(user), httpOptions)
+            .pipe(map(response => {
+                // console.log('response POST register', response);
+                this.isAuthenticated = response.success;
+                if (response.success) {
+                    this.user = response.user;
+                    this.token = response.token;
+                    this.storeData();
+                }
+                return response;
+            }));
+    }
 
-  /* -- Get next lesson & lessons percentage for user progress module -- */
-  fetchNextLesson(lessons: string[]): void {
-    // '605a469942f5481a20c97627' is a test article
-    const nextLessonId = lessons.find(lessonId => lessonId !== '605a469942f5481a20c97627' && !this.user.progress.includes(lessonId));
-    this.dataService.getPostById(nextLessonId).subscribe(
-        (nextLesson) => {
-          this.dataService.dashboard.nextLesson = nextLesson;
-          this.dataService.dashboard.lessonsPercentage = (this.user.progress.length / lessons.length) * 100;
-        }, (error) => {
-          console.log('error while GET next-lesson', error);
-        }
-    );
-  }
+    fetchAllLessons(): void {
+        this.dataService.getAllLessons().subscribe(
+            (lessons) => {
+                this.dataService.dashboard.allLessons = lessons;
+                this.fetchNextLesson(lessons);
+            }, (error) => {
+                console.log('error while GET all-lessons', error);
+            }
+        );
+    }
 
-  editUser(updatedUser: EditUser): Observable<EditUserResponse> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      withCredentials: true
-    };
+    fetchNextLesson(lessons: string[]): void {
+        // '605a469942f5481a20c97627' is a test article
+        const nextLessonId = lessons.find(lessonId => lessonId !== '605a469942f5481a20c97627' && !this.user.progress.includes(lessonId));
 
-    return this.httpClient.patch<EditUserResponse>(this.editUserUrl, JSON.stringify(updatedUser), httpOptions)
-      .pipe(map(response => {
-        // console.log('response PATCH edit-user', response);
-        if (response.success) {
-          this.user = response.user;
-          this.storeData();
-        }
-        return response;
-      }));
-  }
+        this.dataService.getPostById(nextLessonId).subscribe(
+            (nextLesson) => {
+                this.dataService.dashboard.nextLesson = nextLesson;
+                this.dataService.dashboard.lessonsPercentage = (this.user.progress.length / lessons.length) * 100;
+            }, (error) => {
+                console.log('error while GET next-lesson', error);
+            }
+        );
+    }
 
-  addProgress(progress: UserProgress): Observable<AddProgressResponse> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      withCredentials: true
-    };
+    editUser(updatedUser: EditUser): Observable<EditUserResponse> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+            withCredentials: true
+        };
 
-    return this.httpClient.post<AddProgressResponse>(this.addProgressUrl, JSON.stringify(progress), httpOptions)
-      .pipe(map(response => {
-        // console.log('response POST user/add-progress');
-        if (response.success) {
-          this.user = response.user;
-          this.fetchAllLessons();
-        }
-        return response;
-      }));
-  }
+        return this.httpClient.patch<EditUserResponse>(this.EDIT_USER_ENDPOINT, JSON.stringify(updatedUser), httpOptions)
+            .pipe(map(response => {
+                // console.log('response PATCH edit-user', response);
+                if (response.success) {
+                    this.user = response.user;
+                    this.storeData();
+                }
+                return response;
+            }));
+    }
 
-  invalidate(): Observable<LogoutResponse> {
-    const httpOptions = {
-      withCredentials: true
-    };
-    this.isAuthenticated = false;
-    localStorage.removeItem(AUTH_STORAGE_KEY);
-    return this.httpClient.get<LogoutResponse>(this.logoutUrl, httpOptions)
-      .pipe(map(response => {
-          this.user = null;
-          this.token = null;
-          this.isAuthenticated = false;
-          return response;
-      }));
-  }
+    addProgress(progress: UserProgress): Observable<AddProgressResponse> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+            withCredentials: true
+        };
 
-  authenticated(): Observable<AuthenticatedResponse> {
+        return this.httpClient.post<AddProgressResponse>(this.ADD_PROGRESS_ENDPOINT, JSON.stringify(progress), httpOptions)
+            .pipe(map(response => {
+                // console.log('response POST user/add-progress', response);
+                if (response.success) {
+                    this.user = response.user;
+                    this.fetchAllLessons();
+                }
+                return response;
+            }));
+    }
 
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-      withCredentials: true
-    };
+    invalidate(): Observable<LogoutResponse> {
+        const httpOptions = {
+            withCredentials: true
+        };
 
-    return this.httpClient.get<AuthenticatedResponse>(this.authenticatedUrl, httpOptions)
-      .pipe(map(response => {
-          // console.log('response POST authenticated', response);
-          this.isAuthenticated = response.isAuthenticated;
-          if (response.isAuthenticated) {
-            this.user = response.user;
-            this.storeData();
-          }
-          return response;
-      }));
-  }
+        this.isAuthenticated = false;
+        localStorage.removeItem(CREDENTIALS_STORAGE_KEY);
 
-    /* -- Service functions -- */
-    async checkAuthentication(redirectUrl: string = null): Promise<boolean | AuthenticatedResponse> {
-        const stored = localStorage.getItem(AUTH_STORAGE_KEY);
+        return this.httpClient.get<LogoutResponse>(this.LOGOUT_ENDPOINT, httpOptions)
+            .pipe(map(response => {
+                // console.log('response GET user/logout', response);
+                this.user = null;
+                this.token = null;
+                this.isAuthenticated = false;
+                return response;
+            }));
+    }
+
+    authenticated(): Observable<AuthenticatedResponse> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json'
+            }),
+            withCredentials: true
+        };
+
+        return this.httpClient.get<AuthenticatedResponse>(this.AUTHENTICATED_ENDPOINT, httpOptions)
+            .pipe(map(response => {
+                // console.log('response POST authenticated', response);
+                this.isAuthenticated = response.isAuthenticated;
+                if (response.isAuthenticated) {
+                    this.user = response.user;
+                    this.storeData();
+                }
+                return response;
+            }));
+    }
+
+    async checkAuthentication(): Promise<boolean | AuthenticatedResponse> {
+        const stored = localStorage.getItem(CREDENTIALS_STORAGE_KEY);
         if (stored === null) {
             return;
         }
@@ -226,40 +227,39 @@ export class AuthService {
         }
     }
 
-  storeData(): void {
-    localStorage.setItem(AUTH_STORAGE_KEY, JSON.stringify({
-      user: this.user,
-      token: this.token,
-      theme: this.themeService.getActiveTheme().name,
-      isAuthenticated: this.isAuthenticated
-    }));
-  }
+    storeData(): void {
+        localStorage.setItem(CREDENTIALS_STORAGE_KEY, JSON.stringify({
+            user: this.user,
+            token: this.token,
+            theme: this.themeService.getActiveTheme().name,
+            isAuthenticated: this.isAuthenticated
+        }));
+    }
 
-  alreadyRead(lesson: string): boolean {
-    return this.user.progress.includes(lesson);
-  }
+    alreadyRead(lesson: string): boolean {
+        return this.user.progress.includes(lesson);
+    }
 
-  /* -- Component functions -- */
-  setLessonSolved(id): void {
-    const lesson = {
-      userId: this.user._id,
-      postId: id
-    } as UserProgress;
+    setLessonSolved(id): void {
+        const lesson = {
+            userId: this.user._id,
+            postId: id
+        } as UserProgress;
 
-    this.addProgress(lesson).subscribe(
-        (response) => {
-            this.user = response.user;
-            this.storeData();
-            this.matSnackBar.openFromComponent(SnackbarComponent, {
-                duration: 3000,
-                data: 'Lektion als gelesen markiert'
-            });
-        }, (error) => {
-            this.matSnackBar.openFromComponent(SnackbarComponent, {
-                duration: 3000,
-                data: 'Fehler: ' + (typeof error === 'string' ? error : error.message)
-            });
-        }
-    );
-  }
+        this.addProgress(lesson).subscribe(
+            (response) => {
+                this.user = response.user;
+                this.storeData();
+                this.matSnackBar.openFromComponent(SnackbarComponent, {
+                    duration: 3000,
+                    data: 'Lektion als gelesen markiert'
+                });
+            }, (error) => {
+                this.matSnackBar.openFromComponent(SnackbarComponent, {
+                    duration: 3000,
+                    data: 'Fehler: ' + (typeof error === 'string' ? error : error.message)
+                });
+            }
+        );
+    }
 }
