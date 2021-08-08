8e92e5fb93354a5477a80bb3efe4fe37c9e500f2
Sven Brodny <sven.brodny@piobyte.de>
2021-08-08 18:14:41 +0200

    fix: expression changed after checked error due to loading service

--- src/app/app-common/header/header.component.ts
+++ src/app/app-common/header/header.component.ts
@@ -1,28 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 import { HeaderService } from '../../services/header.service';
 import { AuthService } from '../../services/auth/auth.service';
 import { SidenavService } from '../../services/sidenav.service';
+import { LoadingService } from '../../services/loading.service';
+
 
 @Component({
   selector: 'fe-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  @Input() isLoading: boolean;
   @Input() isMobile: boolean;
   @Input() isTiny: boolean;
+  isLoading: boolean;
+  subscription: Subscription = new Subscription();
 
   constructor(public router: Router,
               public headerService: HeaderService,
               public authService: AuthService,
-              public sidenavService: SidenavService
-  ) {
-  }
+              public sidenavService: SidenavService,
+              private loadingService: LoadingService
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.subscription.add(this.loadingService.loading$.pipe(delay(0)).subscribe(value => this.isLoading = value));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   goToSubject(): void {
