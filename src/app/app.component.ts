48c8c9bfb348c16ffa048e6537649ab52b637b24
Sven Brodny <sven.brodny@piobyte.de>
2021-08-20 03:29:58 +0200

    fix: reset scroll position on routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { ViewportScroller } from '@angular/common';
+import { Component, ElementRef } from '@angular/core';
 import { NavigationEnd , Router} from '@angular/router';
 import { filter } from 'rxjs/operators';
 
@@ -15,14 +14,14 @@ export class AppComponent {
   isMobile: boolean;
 
   constructor(private router: Router,
-              private viewportScroller: ViewportScroller,
               private sidenavService: SidenavService,
-              public mediaQueryService: MediaQueryService
+              public mediaQueryService: MediaQueryService,
+              private elementRef: ElementRef
   ) {
     this.mediaQueryService.isMobile$.subscribe(value => this.isMobile = value);
     this.router.events.pipe(filter(event => event instanceof NavigationEnd))
       .subscribe(() => {
-        this.viewportScroller.scrollToPosition([0, 0]);
+        this.elementRef.nativeElement.querySelector('.mat-sidenav-content').scrollTo(0, 0);
         if (this.isMobile) this.sidenavService.close();
       });
   }
