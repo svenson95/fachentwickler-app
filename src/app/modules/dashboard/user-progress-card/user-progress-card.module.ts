9ff32f20bb730a5ab76483c1f5f62be5b18d1382
Sven Brodny <sven.brodny@piobyte.de>
2022-06-08 23:23:23 +0200

    Added skeleton-loader (user-progress next-lesson)

--- src/app/modules/dashboard/user-progress-card/user-progress-card.module.ts
+++ src/app/modules/dashboard/user-progress-card/user-progress-card.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { AngularMaterialModule } from '../../../core/angular-material.module';
 import { CardModule } from '../../../core/components/card/card.module';
-import { LoadingSpinnerModule } from '../../../core/components/loading-spinner/loading-spinner.module';
 import { PostLinkModule } from '../../../shared/components/post-link/post-link.module';
 import { UserProgressCardComponent } from './user-progress-card.component';
 
 @NgModule({
   declarations: [UserProgressCardComponent],
   exports: [UserProgressCardComponent],
-  imports: [CommonModule, CardModule, AngularMaterialModule, PostLinkModule, LoadingSpinnerModule],
+  imports: [CommonModule, AngularMaterialModule, CardModule, PostLinkModule, NgxSkeletonLoaderModule],
 })
 export class UserProgressCardModule {}
