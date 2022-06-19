6f14c4c19dc4c7c011f1692a3caf5595ac273c8c
Sven Brodny <svenbrodny@googlemail.com>
2022-06-19 02:28:21 +0200

    Updated footer component

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { UserService } from '@services/user.service';
+
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  constructor(public router: Router) {}
+  constructor(public router: Router, public user: UserService) {}
 }
