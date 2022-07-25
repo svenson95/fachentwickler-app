a4bf688e7443782430aa5d2614d62ad0070a5fc1
Sven Brodny <svenbrodny@googlemail.com>
2022-07-25 02:52:39 +0200

    Changed router-loading-spinner to app-icon

--- src/app/core/components/main/main.module.ts
+++ src/app/core/components/main/main.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 
+import { AppIconModule } from '@core-components/app-icon/app-icon.module';
+
 import { SharedModule } from '../../../shared/shared.module';
 import { FooterModule } from '../footer/footer.module';
 
@@ -8,6 +10,6 @@ import { MainComponent } from './main.component';
 @NgModule({
   declarations: [MainComponent],
   exports: [MainComponent],
-  imports: [SharedModule, FooterModule],
+  imports: [SharedModule, FooterModule, AppIconModule],
 })
 export class MainModule {}
